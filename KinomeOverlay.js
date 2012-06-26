3ecc5e5815c1d9e82feaae6bf4c06a94fcdf3802
Joseph Lee <josel@pdx.edu>
2012-06-26 00:26:26 -0700

    Initial commit New repo on googlecode, originally from github.com/josephl/KinomeOverlay
    Signed-off-by: Joseph Lee <josel@pdx.edu>

---
+++ KinomeOverlay.js
@@ -0,0 +1,145 @@
+var KO= {};     // KO-global scope
+
+KO.div = d3.select("body").append("div")
+    .attr("id", "KOdiv")
+    //.attr("width", 825)
+    .attr("height", 975);
+
+KO.opac = 0.55;      // fill-opacity of scatterplot points
+
+KO.svg = KO.div     // main svg element
+    .append("svg")
+    .attr("width", 825)
+    .attr("height", 975)
+    .style("background-image", "url(img/KinomePoster4.jpg)")
+    .style("background-size", "100% 100%");
+
+KO.scale = 0.25;    // scaling factor of image (vs original 3300x4956 jpg
+
+KO.coords = [];     // closure
+KO.points = {};
+
+/* populate KO.coords table
+ * import coords.json file */
+d3.json("kotable.json", function(json) {
+    KO.coords = json;
+    /* generate static kinase endpoints */
+    KO.points =  KO.svg.selectAll("circle")
+        .data(KO.coords)
+        .enter()
+        .append("circle")
+        .attr("cx", function(d) { return parseFloat(d.xcoord) * KO.scale; })
+        .attr("cy", function(d) { return parseFloat(d.ycoord) * KO.scale; })
+        .attr("r", 2)
+        .style("fill", "white")
+        .style("stroke", 1);
+});
+
+/* return x-coord given input geneid string
+ */
+KO.getCoordX = function(geneid_in) {
+    for (i = 0; i < KO.coords.length; i++) {
+        if (KO.coords[i].GeneID == geneid_in)        // change to geneID
+            return parseInt(KO.coords[i].xcoord) * KO.scale;
+    }
+    return null;    // geneID not found
+};
+
+/* return y-coord given input geneid string
+ */
+KO.getCoordY = function(geneid_in) {
+    for (i = 0; i < KO.coords.length; i++) {
+        if (KO.coords[i].GeneID == geneid_in)        // change to geneID
+            return parseInt(KO.coords[i].ycoord) * KO.scale;
+    }
+    return null;    // geneID not found
+};
+
+KO.reader = new FileReader();
+KO.iValPtGrp = [];
+
+/* on file upload */
+KO.inputFileHandler = function(evt) {
+
+    //var files = evt.target.files;   // FileList obj
+    var files = document.getElementById("KOfiles").files;
+
+    // closure, needed to read in file
+    KO.reader.onload = function(e) {
+        // Parse csv file
+        var csvfile = d3.csv.parse(KO.reader.result);
+        KO.iValPtGrp.push(KO.svg
+                .append("g")
+                .attr("id", KO.iValPtGrp.length));
+        KO.iValPtGrp[KO.iValPtGrp.length - 1].selectAll("circle")
+            .data(csvfile)
+            .enter()
+            .append("circle")
+            .on("mouseover", function(d) {
+                d3.select(this).style("fill-opacity", .8);
+                KO.TooltipGrp.attr("visibility", "visible");
+                // KO.Tooltip.attr("x", event.pageX + 20)
+                // .attr("y", event.pageY - 10);
+                KO.TooltipText
+                // .attr("x", event.pageX + 30)
+                // .attr("y", event.pageY - 20)
+                .attr("x", parseFloat(KO.getCoordX(d.GeneID)) + 20)
+                .attr("y", parseFloat(KO.getCoordY(d.GeneID)))
+                .text(function() {
+                    return "Intensity: " + d.intensityVal; });
+            })
+            .on("mouseout", function() {
+                d3.select(this).style("fill-opacity", KO.opac);
+                KO.TooltipGrp.attr("visibility", "hidden");
+            })
+            .attr("id", function(d) { return d.GeneID; })
+            .style("fill", function(d) {
+                if (parseFloat(d.intensityVal) < 0) {
+                    return "red";
+                }
+                else {
+                    return "green";
+                }
+            })
+            .style("fill-opacity", KO.opac)
+            .attr("cx", function(d) {
+                //console.log(KO.getCoordX([d.geneID]));
+                return KO.getCoordX(d.GeneID);
+            })
+            .attr("cy", function(d) {
+                //console.log(KO.getCoordY([d.geneID]));
+                return KO.getCoordY(d.GeneID);
+            })
+            .attr("r", function(d) {
+                return 7 * (2.5 ^ Math.abs(parseFloat(d.intensityVal)));
+            });
+
+    KO.TooltipGrp = KO.svg.append("g")
+        .attr("visibility", "hidden");
+/*  remove tooltip palette?
+    KO.Tooltip = KO.TooltipGrp.append("rect")
+    .attr("width", 100)
+    .attr("height", 100)
+    .style("fill", "white")
+    .style("fill-opacity", .9)
+    .style("stroke", 2);
+*/
+    KO.TooltipText = KO.TooltipGrp.append("text")
+    .text("Testing tooltip")
+    .attr("fill", "black")
+    .attr("font-family", "sans-serif")
+    .attr("font-size", "14px");
+
+    };
+
+    KO.reader.readAsText(files[0]);
+
+};
+document.getElementById("KOfiles").addEventListener("change", KO.inputFileHandler, false);
+
+/*
+ * TOREAD:
+ * http://www.ncbi.nlm.nih.gov/pmc/articles/PMC3361704/?tool=pubmed
+ * http://rnai.nih.gov/haystack/
+ * http://jbx.sagepub.com/content/17/4/496.full
+ */
